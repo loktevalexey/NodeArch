fd2cd7d2c871cd372babda33774d6ab3fa173c17
loktev <loktev@tut.by>
2019-08-01 22:15:20 +0300

    ex3050

--- server3050/server.js
+++ server3050/server.js
@@ -2,7 +2,6 @@
 
 const webserver = express(); // создаём веб-сервер
 
-webserver.use(express.json()); // учим сервер распознавать тела POST-запросов с заголовком Content-Type: application/json
 webserver.use(express.urlencoded()); // учим сервер распознавать тела POST-запросов с заголовком Content-Type: application/x-www-form-urlencoded
 
 const port = 3050;
@@ -21,6 +20,7 @@ webserver.get('/service2', (req, res) => {
 
 webserver.post('/service3', (req, res) => { 
     // при обращении по этому УРЛу - ответ зависит от POST-данных
+    // миддлварь urlencoded раскодировала данные POST-запроса и положила в req.body
     console.log('service3 called, req.body=',req.body);
     res.send("service3 ok, login="+req.body.login+" age="+req.body.age);
 });
