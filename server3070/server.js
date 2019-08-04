29dacf7be6c4931d77e5e1daa62c4f6c58574d37
loktev <loktev@tut.by>
2019-08-04 10:48:20 +0300

    nodemon added; file log in server dir

--- server3070/server.js
+++ server3070/server.js
@@ -1,6 +1,14 @@
 ﻿const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const os = require('os');
+
+const webserver = express();
+
+webserver.use(express.urlencoded({extended:true}));
+
+const port = 3070;
+const logFN = path.join(__dirname, '_server.log');
 
 // пишет строку в файл лога и одновременно в консоль
 // origin - программа или модуль, который выводит строку
@@ -16,48 +24,42 @@ function logLineSync(logFilePath,logLine) {
     fs.closeSync(logFd);
 }
 
-const webserver = express();
-
-webserver.use(express.urlencoded({extended:true}));
-
-const port = 3070;
-
 webserver.get('/service1', (req, res) => { 
     // при обращении по этому УРЛу - просто отдаём строку
-    logLineSync('_server.log','service1 called');
+    logLineSync(logFN,'service1 called');
     res.send("service1 ok!");
 });
 
 webserver.get('/service2', (req, res) => { 
     // при обращении по этому УРЛу - ответ зависит от GET-параметров
-    logLineSync('_server.log',"service2 called, get pars: "+JSON.stringify(req.query));
+    logLineSync(logFN,"service2 called, get pars: "+JSON.stringify(req.query));
     res.setHeader("X-XSS-Protection", "0"); // добавляем в ответ заголовок, иначе неинтересно
     res.send("service2 ok, par1="+req.query.par1+" par2="+req.query.par2);
 });
 
 webserver.get('/service3', (req, res) => { 
     // при обращении по этому УРЛу - ответ всегда ошибка 401
-    logLineSync('_server.log','service3 called');
+    logLineSync(logFN,'service3 called');
     res.status(401).end();
 });
 
 webserver.get('/service4', (req, res) => { 
     // при обращении по этому УРЛу - ответ всегда ошибка 401 и в качестве тела ответа - текст ошибки
-    logLineSync('_server.log','service4 called');
+    logLineSync(logFN,'service4 called');
     res.status(401).send("sorry, access denied!");
 });
 
 webserver.get('/service5', (req, res) => { 
     // при обращении по этому УРЛу - ответа просто не будет
-    logLineSync('_server.log','service5 called');
+    logLineSync(logFN,'service5 called');
 });
 
 webserver.post('/service6', (req, res) => { 
     // при обращении по этому УРЛу - ответ зависит от POST-данных
     // миддлварь urlencoded раскодировала данные POST-запроса и положила в req.body
-    logLineSync('_server.log','service6 called, req.body: '+JSON.stringify(req.body));
+    logLineSync(logFN,'service6 called, req.body: '+JSON.stringify(req.body));
     res.send("service6 ok, login="+req.body.login+" age="+req.body.age);
 });
 
 webserver.listen(port);
-logLineSync('_server.log',"web server running on port "+port);
+logLineSync(logFN,"web server running on port "+port);
---
+++ servers/servers.js
@@ -0,0 +1,3 @@
+require("../server3050/server");
+require("../server3060/server");
+require("../server3070/server");
