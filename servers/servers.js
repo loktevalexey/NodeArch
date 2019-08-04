c988cccde110f34d98758d9f6088047cbf5d31f7
loktev <loktev@tut.by>
2019-08-04 20:10:39 +0300

    3080

--- servers/servers.js
+++ servers/servers.js
@@ -1,6 +1,7 @@
 require("../theme30/server3050/server3050");
 require("../theme30/server3060/server3060");
 require("../theme30/server3070/server3070");
+require("../theme30/server3080/server3080");
 
 require("../theme35/server3533/server3533");
 require("../theme35/server3535/server3535");
---
+++ theme30/server3080/server3080.js
@@ -0,0 +1,55 @@
+﻿const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+const webserver = express();
+
+webserver.use(express.urlencoded({extended:true}));
+
+const port = 3080;
+const logFN = path.join(__dirname, '_server.log');
+
+// пишет строку в файл лога и одновременно в консоль
+// origin - программа или модуль, который выводит строку
+function logLineSync(logFilePath,logLine) {
+    const logDT=new Date();
+    let time=logDT.toLocaleDateString()+" "+logDT.toLocaleTimeString();
+    let fullLogLine=time+" "+logLine;
+
+    console.log(fullLogLine); // выводим сообщение в консоль
+
+    const logFd = fs.openSync(logFilePath, 'a+'); // и это же сообщение добавляем в лог-файл
+    fs.writeSync(logFd, fullLogLine + os.EOL); // os.EOL - это символ конца строки, он разный для разных ОС
+    fs.closeSync(logFd);
+}
+
+webserver.get('/service1', (req, res) => { 
+    logLineSync(logFN,"service1 called, get pars: "+JSON.stringify(req.query));
+    res.setHeader("X-XSS-Protection", "0"); // добавляем в ответ специальный заголовок, чтобы отключить защитный механизм в Chrome
+    res.send("service1 ok, par1="+req.query.par1+" par2="+req.query.par2);
+});
+
+webserver.get('/service2', (req, res) => { 
+    logLineSync(logFN,"service2 called, get pars: "+JSON.stringify(req.query));
+    
+    let par1=parseInt(req.query.par1)||0;
+    let par2=escapeHTML(req.query.par2);
+
+    res.send("service2 ok, par1="+par1+" par2="+par2);
+});
+
+webserver.listen(port);
+logLineSync(logFN,"web server running on port "+port);
+
+function escapeHTML(text) {
+    if ( !text )
+        return text;
+    text=text.toString()
+        .split("&").join("&amp;")
+        .split("<").join("&lt;")
+        .split(">").join("&gt;")
+        .split('"').join("&quot;")
+        .split("'").join("&#039;");
+    return text;
+}
