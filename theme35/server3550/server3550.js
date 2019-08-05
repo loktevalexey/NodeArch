37b6eb6547c8f0966f96979568504eaf34961e06
loktev <loktev.alex.74@gmail.com>
2019-08-05 15:48:46 +0300

    3550

--- theme35/server3550/server3550.js
+++ theme35/server3550/server3550.js
@@ -8,7 +8,8 @@ const webserver = express();
 
 webserver.use(express.urlencoded({extended:true}));
 
-const upload = multer( { dest: path.join(__dirname,"uploads") } ); // миддлварь для работы с multipart/form-data; если потребуется сохранение загруженных файлов - то в папку uploads
+// миддлварь для работы с multipart/form-data; если потребуется сохранение загруженных файлов - то в папку uploads
+const upload = multer( { dest: path.join(__dirname,"uploads") } ); 
 
 const port = 3550;
 const logFN = path.join(__dirname, '_server.log');
@@ -41,8 +42,9 @@ webserver.post('/service3', upload.none(), (req, res) => { // миддлварь
     res.send("ok login="+req.body.login);
 });
 
-const service4files = upload.fields( [ {name:'photo', maxCount:1} ] );
-webserver.post('/service4', service4files, (req, res) => { // миддлварь, сохраняющая файл, прилетевший под именем photo
+// миддлварь, разбирающая данные формы в формате multipart/form-data и сохраняющая файл, прилетевший под именем photo
+const service4files = upload.fields( [ {name:'photo', maxCount:1} ] ); 
+webserver.post('/service4', service4files, (req, res) => { 
     logLineSync(logFN,"service4 called");
 
     console.log("request post data",req.body);
