0a1f81ecde068e7a5f88281b7d99a0e47a85b320
loktev <loktev.alex.74@gmail.com>
2019-08-07 16:30:44 +0300

    3570

---
+++ theme35/server3570/server3570.js
@@ -0,0 +1,40 @@
+﻿const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const { logLineSync } = require('../../utils/utils');
+
+const webserver = express();
+
+const port = 3570;
+const logFN = path.join(__dirname, '_server.log');
+
+webserver.get('/service1', (req, res) => { 
+    logLineSync(logFN,"service1 called");
+
+    res.setHeader("Content-Type", "text/plain; charset=UTF-8");
+
+    var readStream=fs.createReadStream( path.join(__dirname,"files/text_utf8.txt") );
+    readStream.pipe(res);
+});
+
+webserver.get('/service2', (req, res) => { 
+    logLineSync(logFN,"service2 called");
+
+    res.setHeader("Content-Type", "text/plain; charset=windows-1251");
+
+    var readStream=fs.createReadStream( path.join(__dirname,"files/text_win1251.txt") );
+    readStream.pipe(res);
+});
+
+webserver.get('/service3', (req, res) => { 
+    logLineSync(logFN,"service3 called");
+
+    res.setHeader("Content-Type", "text/plain; charset=UTF-8");
+
+    var readStream=fs.createReadStream( path.join(__dirname,"files/text_win1251.txt") );
+    readStream.pipe(res);
+});
+
+webserver.listen(port);
+logLineSync(logFN,"web server running on port "+port);
