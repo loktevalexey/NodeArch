e7120e76b99938d5438ede6fe0830883cc394d94
loktev <loktev.alex.74@gmail.com>
2019-08-14 13:40:50 +0300

    4035

---
+++ theme40/server4035/server4035.js
@@ -0,0 +1,29 @@
+﻿const express = require('express');
+const path = require('path');
+
+const { logLineSync } = require('../../utils/utils');
+
+const webserver = express();
+
+const port = 4035;
+const logFN = path.join(__dirname, '_server.log');
+
+webserver.use(function (req, res, next) {
+    logLineSync(logFN,"static server called, originalUrl="+req.originalUrl);
+    next();
+});
+
+webserver.use(function(req, res, next) {
+    if ( /\.html$/.test(req.path) ) {
+      res.setHeader("Content-Type","text/html; charset=windows-1251");
+    }
+    next();
+  });
+  
+webserver.use(
+    "/mysite",
+    express.static(path.resolve(__dirname,"../site_football"))
+);
+
+webserver.listen(port);
+logLineSync(logFN,"web server running on port "+port);
