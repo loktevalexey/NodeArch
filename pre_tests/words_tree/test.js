5a6aaec4120f0f81bd4993bc75f8a782a5de5efb
loktev <loktev.alex.74@gmail.com>
2019-08-29 11:24:46 +0300

    pre-test

---
+++ pre_tests/words_tree/test.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+let results=[];
+
+function checkFile(filename) {
+    try {
+        let txt=fs.readFileSync(filename,"utf8");
+        try {
+            let files=JSON.parse(txt);
+            files.forEach( file => checkFile(file) );
+        }
+        catch ( er ) {
+            results.push(txt);
+        }
+    }
+    catch ( er ) {
+        console.log(filename+" skipped");
+    }
+    
+}
+
+checkFile("root.txt");
+console.log(results.join(" "));
