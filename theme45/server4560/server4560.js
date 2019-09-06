30c9f155f468bb52b351e4ced199319cf8cdeeaa
loktev <loktev.alex.74@gmail.com>
2019-09-06 15:57:13 +0300

    4560

---
+++ theme45/server4560/server4560.js
@@ -0,0 +1,21 @@
+﻿const express = require('express');
+const path = require('path');
+
+const { logLineAsync } = require('../../utils/utils');
+
+const webserver = express();
+
+const port = 4560;
+const logFN = path.join(__dirname, '_server.log');
+
+webserver.use(function (req, res, next) {
+    logLineAsync(logFN,"static server called, originalUrl="+req.originalUrl);
+    next();
+});
+
+webserver.use(
+    express.static(path.resolve(__dirname,"static"))
+);
+
+webserver.listen(port);
+logLineAsync(logFN,"web server running on port "+port);
---
+++ theme45/server4560/static/script.js
@@ -0,0 +1,9 @@
+import { PROGNAME } from "./script2.js";
+
+function test(x) {
+    return PROGNAME+" "+(x*x);
+}
+
+export { 
+    test 
+};
