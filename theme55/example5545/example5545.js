58425db43efd7a7563fa8b6b11909d831c8150c8
loktev <loktev.alex.74@gmail.com>
2019-09-10 15:52:12 +0300

    5545 5546

---
+++ theme55/example5545/example5545.js
@@ -0,0 +1,16 @@
+﻿const path = require('path');
+const fs = require('fs');
+
+var stats="";
+
+var readStream=fs.createReadStream( path.join(__dirname,"data.txt") );
+readStream.on('data', chunk => {
+    stats+=chunk;
+    console.log("read "+chunk.length+" bytes");
+});
+readStream.on('end',()=>{
+    console.log("all read - "+stats.length+" bytes");
+});
+readStream.on('error', err =>{
+    console.log("ERROR!",err);
+});
---
+++ theme55/example5546/example5546.js
@@ -0,0 +1,21 @@
+﻿const EventEmitter = require('events');
+
+class MyTimer extends EventEmitter {
+
+    constructor(interval) {
+        super();
+
+        let count=0;
+        setInterval(()=>{
+            count++;
+            this.emit('timer',count);
+        },interval);
+
+    }
+
+}
+
+const myTimer = new MyTimer(1000);
+myTimer.on('timer', count => {
+    console.log("таймер сработал: "+count);
+} );
