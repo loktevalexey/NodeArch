24588ab21ae52ac4f544601a752bf6c54adf2220
loktev <loktev.alex.74@gmail.com>
2019-09-13 14:52:47 +0300

    5574 5577

---
+++ theme55/example5574/hello.js
@@ -0,0 +1,18 @@
+﻿const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question(`Ваша фамилия? `, fam => {
+    
+    rl.question(`Ваш возраст? `, age => {
+
+        console.log(`Привет, ${fam}, тебе ${age} лет!`);
+
+        rl.close();
+    });
+        
+});
+
---
+++ theme55/example5577/repl.js
@@ -0,0 +1,25 @@
+﻿const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'GREETER> '
+});
+
+rl.prompt(); // выдать prompt и ждать ответа
+rl.on('line', name => {
+
+    if ( !name )  
+        rl.close(); // если введена пустая строка - прерываем цикл REPL, и сработает событие close
+    else {  
+        console.log(`Привет, ${name}`);
+        rl.prompt();  // снова выдать prompt и ждать ответа
+    }
+
+});
+
+rl.on('close', () => {
+    // сюда придём либо по Ctrl+C, либо по вводу пустой строки в ответ на prompt
+    console.log('До свидания!');
+    process.exit(0); // выход с признаком успешного завершения
+});
