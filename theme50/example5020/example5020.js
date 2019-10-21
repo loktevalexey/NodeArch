fcf49bfde22cfb2387103b5c7dde0a50b249479a
loktev <loktev@tut.by>
2019-10-21 22:18:21 +0300

    5020

---
+++ theme50/example5020/example5020.js
@@ -0,0 +1,7 @@
+﻿const path = require('path');
+
+const { logLineAsync } = require('../../utils/utils');
+
+const logFN = path.join(__dirname, '_example.log');
+
+logLineAsync(logFN,"example5020 executed!");
