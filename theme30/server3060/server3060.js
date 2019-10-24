94e4bb34c7a2249b699913a45ce1d135fc733812
loktev <loktev@tut.by>
2019-10-24 08:37:22 +0300

    слайды

--- theme30/server3060/server3060.js
+++ theme30/server3060/server3060.js
@@ -8,31 +8,9 @@ const port = 3060;
 
 webserver.get('/service1', (req, res) => { 
     // при обращении по этому УРЛу - просто отдаём строку
+    // миддлварь urlencoded ничего не меняла, она реагирует на конкретный формат запроса - application/x-www-form-urlencoded
     console.log('service1 called');
     res.send("service1 ok!");
-});
-
-webserver.get('/service2', (req, res) => { 
-    // при обращении по этому УРЛу - ответ зависит от GET-параметров
-    console.log('service2 called, req.query=',req.query);
-    res.send("service2 ok, par1="+req.query.par1+" par2="+req.query.par2);
-});
-
-webserver.get('/service3', (req, res) => { 
-    // при обращении по этому УРЛу - ответ всегда ошибка 401
-    console.log('service3 called');
-    res.status(401).end();
-});
-
-webserver.get('/service4', (req, res) => { 
-    // при обращении по этому УРЛу - ответ всегда ошибка 401 и в качестве тела ответа - текст ошибки
-    console.log('service4 called');
-    res.status(401).send("sorry, access denied!");
-});
-
-webserver.get('/service5', (req, res) => { 
-    // при обращении по этому УРЛу - ответа просто не будет
-    console.log('service5 called');
 });
 
 webserver.post('/service6', (req, res) => { 
