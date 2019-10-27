d2ddbea78b1aca244719d9ef657f0760b0e24a4c
loktev <loktev@tut.by>
2019-10-27 20:41:25 +0300

    slides

--- theme35/server3585/server3585.js
+++ theme35/server3585/server3585.js
@@ -18,7 +18,7 @@ webserver.post('/service1', (req, res) => {
 
 webserver.options('/service2', (req, res) => { 
     logLineSync(logFN,"service2 preflight called");
-    res.setHeader("Access-Control-Allow-Origin","*"); // разрешаем обращаться с любого origin
+    res.setHeader("Access-Control-Allow-Origin","*"); // разрешаем обращаться с любого origin, вместо * здесь может быть ОДИН домен
     res.setHeader("Access-Control-Allow-Headers","Content-Type"); // разрешаем заголовок запроса Content-Type
     // в HTTP есть код ответа 204 - "пустой ответ", здесь именно это и нужно
     // но не все браузеры понимают код ответа 204 на запрос OPTIONS, лучше вернуть код ответа 200 и пустой ответ
@@ -32,7 +32,7 @@ webserver.post('/service2', (req, res) => {
 });
 
 var CORSOptions = {
-    origin: '*', // разрешаем обращаться с любого origin
+    origin: '*', // разрешаем обращаться с любого origin, вместо * здесь может быть ОДИН домен
     optionsSuccessStatus: 200, // на OPTIONS отсылать код ответа 200
 };
 webserver.options('/service3', cors(CORSOptions));  // включаем мидлварь cors в цепочку мидлварей, причём ответ не нужно прописывать - мидлварь его сама отправит
