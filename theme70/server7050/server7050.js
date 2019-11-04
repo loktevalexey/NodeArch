25d4a78374109008194d466c651b77804d6cd4ac
loktev <loktev@tut.by>
2019-11-04 23:00:28 +0300

    7050

--- theme70/server7050/server7050.js
+++ theme70/server7050/server7050.js
@@ -14,11 +14,6 @@ const logFN = path.join(__dirname, '_server.log');
 
 import CoreApp from './src/CoreApp';
 
-// когда из браузера загружаются js- и css-бандлы, они отдаются напрямую из папки build как обычная статика
-webserver.use(
-    express.static(path.resolve(__dirname, 'build'), { maxAge: '30d' })
-);
-
 const serverRenderer = (req, res) => {
     fs.readFile(path.resolve(__dirname, 'src','index.html'), 'utf8', (err, indexStr) => {
         if (err) {
@@ -44,7 +39,26 @@ const serverRenderer = (req, res) => {
             )
         );
     });
-}
+};
+
+/*
+webserver.use(function (req, res, next) {
+    logLineAsync(logFN,`[${port}] `+"server called, originalUrl="+req.originalUrl);
+    next();
+});
+*/
+
+// если обращение идёт УРЛу / - запускаем SSR
+webserver.use('/', serverRenderer);
+// на самом деле, serverRenderer готов обработать любой УРЛ, но мы ПЕРЕД ним должны через middleware static отдать бандлы из папки build
+// а там есть файл index.html
+// а при обращении к УРЛу "/" middleware static отдаёт этот index.html, считая его совпадающим с этим УРЛ
+// поэтому ещё до middleware static отдельно указываем что именно этот УРЛ обрабатывается serverRenderer
+
+// когда из браузера загружаются js- и css-бандлы, они отдаются напрямую из папки build как обычная статика
+webserver.use(
+    express.static(path.resolve(__dirname, 'build'), { maxAge: '30d' })
+);
 
 // если обращение идёт к любому другому УРЛу - запускаем SSR
 webserver.use('*', serverRenderer);
