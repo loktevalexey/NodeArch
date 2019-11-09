6f63343afc43c1fea5a86610a224c758f6ee8ec2
loktev <loktev@tut.by>
2019-11-09 23:55:43 +0300

    7690

---
+++ theme75/server7691/server7691.js
@@ -0,0 +1,23 @@
+﻿const express = require('express');
+const path = require('path');
+const fsp = require('fs').promises;
+
+const { logLineAsync } = require('../../utils/utils');
+
+const webserver = express();
+
+const port = 7691;
+const logFN = path.join(__dirname, '_server.log');
+
+webserver.get('/getsportpage/:sportname', async function (req, res) {
+    logLineAsync(logFN,`[${port}] url=${req.originalUrl} sportname=${req.params.sportname}`);
+    
+    let fileContents=await fsp.readFile(path.join(__dirname, '..','site_football3','page_pattern.html'),"utf8");
+    fileContents=fileContents.split("$$$SPORT$$$").join(req.params.sportname);
+    fileContents=fileContents.split("$$$TIME$$$").join((new Date()).toLocaleString());
+
+    res.send(fileContents);
+});
+
+webserver.listen(port);
+logLineAsync(logFN,"web server running on port "+port);
