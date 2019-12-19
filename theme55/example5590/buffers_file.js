2f7eb3de4309c9688ba2697c4c9abe05c39d30a0
loktev <loktev@tut.by>
2019-12-19 21:24:32 +0300

    5590

--- theme55/example5590/buffers_file.js
+++ theme55/example5590/buffers_file.js
@@ -5,12 +5,12 @@ const path = require("path");
 // но мы-то знаем, что в файле лежит utf8-строка, поэтому сразу говорим - представь нам прочитанное в виде utf8-строки
 let txt_data=fs.readFileSync(path.resolve(__dirname,"data","data_utf8.txt"),"utf8");
 console.log("текстовые данные в запрошенной кодировке:");
-console.log(txt_data);
+console.log(txt_data,txt_data.length);
 
 // если явно не запросить представление в виде строки - получим просто буфер, массив байтов
 let buffer_data=fs.readFileSync(path.resolve(__dirname,"data","data_utf8.txt"));
 console.log("данные в виде буфера:");
-console.log(buffer_data);
+console.log(buffer_data,buffer_data.length);
 // кроме букв, видим в начале строки байты ef bb bf - это utf8-символ BOF, begin of file
 
 // буфер можно преобразовать в строку, по-умолчанию предполагается что буфер представляет байты utf8-строки
