8dfa3270a87af4a742a72b9469a9e3326b5671f1
loktev <loktev@tut.by>
2019-12-19 21:31:23 +0300

    5545

--- theme55/example5545/example5545.js
+++ theme55/example5545/example5545.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 
 var stats="";
 
-var readStream=fs.createReadStream( path.join(__dirname,"data.txt") );
+var readStream=fs.createReadStream( path.join(__dirname,"data.txt"), "utf8" );
 readStream.on('data', chunk => {
     stats+=chunk;
-    console.log("read "+chunk.length+" bytes");
+    console.log("read "+chunk.length+" chars");
 });
 readStream.on('end',()=>{
-    console.log("all read - "+stats.length+" bytes");
+    console.log("all read - "+stats.length+" chars");
 });
 readStream.on('error', err =>{
     console.log("ERROR!",err);
