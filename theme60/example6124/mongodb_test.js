03b9b0c0d227185683cad1d7c67bba5e2a159b27
loktev <loktev.alex.74@gmail.com>
2020-01-13 11:38:51 +0300

    6124

---
+++ theme60/example6124/mongodb_test.js
@@ -0,0 +1,41 @@
+const MongoClient = require('mongodb').MongoClient;
+ 
+const serverURL = 'mongodb://localhost:27017'; // УРЛ сервера Mongo
+ 
+const databaseName = 'learning'; // имя базы данных
+ 
+MongoClient.connect(serverURL, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
+    if ( err )
+      console.error(err);
+    else {
+        console.log("Connected successfully to server");
+    
+        const database = client.db(databaseName); // получаем ссылку на базу данных
+
+        const studentsCollection = database.collection('Student'); // получаем ссылку на коллекцию
+        
+        // вставляем новые документы
+        studentsCollection.insertMany([ {name:'Сидоров', height:185}, {name:'Григорьев', height:192} ], function(err, result) {
+            if ( err )
+                console.error(err);
+            else {
+                console.log("insert done", result);
+
+                studentsCollection.find({}).toArray(function(err, docs) {
+                    if ( err )
+                        console.error(err);
+                    else {
+                        console.log("students found:");
+                        console.log(docs);
+
+                        client.close(); // закрываем соединение с БД
+                    }
+                  });
+
+            }
+        });
+
+        
+    }
+    
+});
