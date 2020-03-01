8ac196d20eb9790e367178d5cf4021a19bf43dad
loktev <loktev@tut.by>
2020-03-01 20:13:59 +0300

    cloned

---
+++ _init.bat
@@ -0,0 +1,7 @@
+git init
+git config credential.helper store
+git add .
+git commit -m "cloned"
+git remote add origin https://github.com/loktevalexey/NodeArch-V2.git
+git push -u origin master
+git status
